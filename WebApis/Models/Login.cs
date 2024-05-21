7f4f68eb68fc4ab4f02920339ca4387963c621d4
= <=>
2024-05-21 15:12:34 -0300

    Criação da API principal e suas classes principais.

---
+++ WebApis/Models/Login.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApis.Models
+{
+    public class Login
+    {
+        public string email { get; set; }
+        public string senha { get; set; }
+        public string cpf { get; set; }
+    }
+}
---
+++ WebApis/Models/MessageViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApis.Models
+{
+    public class MessageViewModel
+    {
+        public int Id { get; set; }
+
+        public string Titulo { get; set; }
+
+        public bool Ativo { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+
+        public DateTime DataAlteracao { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
